00ce7cb98dee8e12c1b4ea9e5a43833e24e0444b
leshayq <oleshadesigner123@gmail.com>
2024-10-03 20:15:44 +0300

    pr3

--- main.js
+++ main.js
@@ -1,48 +1,84 @@
-function firstScript() {
-    const firstRow = prompt('Введіть перший рядок:');
-    const secondRow = prompt('Введіть другий рядок:');
-    const letter = prompt('Введіть літеру для підрахунку:');
+const $btnKick = document.getElementById('btn-kick');
+const $btnHeavyKick = document.getElementById('btn-heavy-kick');
 
-    function countLetter(str, letter) {
-        let count = 0;
-        for (let i = 0; i < str.length; i++) {
-            if (str.charAt(i) === letter) {
-                count++;
-            }
-        }
-        return count;
-    }
+const character = {
+    name: 'Pikachu',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-character'),
+    elProgressbarHP: document.getElementById('progressbar-character'),
+  };
+  
+const enemy = {
+    name: 'Charmander',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-enemy'),
+    elProgressbarHP: document.getElementById('progressbar-enemy'),
+};
 
-    function compareString(firstRow, secondRow, letter) {
-        const countFirstRow = countLetter(firstRow, letter);
-        const countSecondRow = countLetter(secondRow, letter);
+const enemy2 = {
+    name: 'Mewtwo',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-enemy2'),
+    elProgressbarHP: document.getElementById('progressbar-enemy2'),
+};
 
-        return countFirstRow > countSecondRow ? firstRow : secondRow;
-    }
+$btnKick.addEventListener('click', function () {
+    console.log('Kick');
+    attack(random(20), character);
+    attack(random(20), enemy);
+    attack(random(20), enemy2);
+});
 
-    const result = compareString(firstRow, secondRow, letter);
-    alert('Більше літер "' + letter + '" у рядку: "' + result + '"');
+$btnHeavyKick.addEventListener('click', function () {
+    console.log('Heavy Kick');
+    attack(random(40), character);
+    attack(random(40), enemy);
+    attack(random(40), enemy2);
+});
+
+function init() {
+    console.log('Start game!');
+    renderHP(character);
+    renderHP(enemy);
+    renderHP(enemy2);
 }
-function secondScript() {
-    function formattedPhone(phone) {
-        let cleanedPhone = phone.replace(/\D/g, '');
 
-        if (cleanedPhone.length === 10) {
-            cleanedPhone = '38' + cleanedPhone;
-        } else if (cleanedPhone.length === 11 && cleanedPhone.startsWith('8')) {
-            cleanedPhone = '3' + cleanedPhone;
-        } else if (cleanedPhone.length === 12 && cleanedPhone.startsWith('38')) {
-        } else {
-            return 'Неправильний формат номера телефону';
-        }
-
-        return `+${cleanedPhone.slice(0, 2)} (${cleanedPhone.slice(2, 5)}) ${cleanedPhone.slice(5, 8)}-${cleanedPhone.slice(8, 10)}-${cleanedPhone.slice(10)}`;
-    }
-
-    const phoneInput = prompt('Введіть номер телефону:');
-    const result = formattedPhone(phoneInput);
-    alert(result);
+function renderHP(person) {
+    renderHPLife(person);
+    renderProgressbarHP(person);
 }
 
-document.getElementById('button1').onclick = firstScript;
-document.getElementById('button2').onclick = secondScript;
+function renderHPLife(person) {
+    person.elHP.innerText = person.damageHP + ' / ' + person.defaultHP;
+}
+
+function renderProgressbarHP(person) {
+    person.elProgressbarHP.style.width = (person.damageHP / person.defaultHP) * 100 + '%';
+
+    person.elProgressbarHP.classList.remove('low', 'critical');
+
+    if (person.damageHP > 60) {
+    } else if (person.damageHP > 30) {
+        person.elProgressbarHP.classList.add('low'); 
+    } else {
+        person.elProgressbarHP.classList.add('critical'); 
+    }
+}
+
+function attack(damage, person) {
+    if (person.damageHP < damage) {
+        person.damageHP = 0;
+        alert('Бідний ' + person.name + ' програв бій!');
+    } else {
+        person.damageHP -= damage;
+    }
+    renderHP(person);
+}
+
+function random(num) {
+    return Math.ceil(Math.random() * num);
+}
+init();
