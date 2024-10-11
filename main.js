b3548fc1c537fd881aad4f3f4a1339191d38b0e9
leshayq <oleshadesigner123@gmail.com>
2024-10-11 09:49:48 +0300

    pr3_2

--- main.js
+++ main.js
@@ -7,14 +7,46 @@ const character = {
     damageHP: 100,
     elHP: document.getElementById('health-character'),
     elProgressbarHP: document.getElementById('progressbar-character'),
-  };
-  
+    renderHP: function () {
+        this.renderHPLife();
+        this.renderProgressbarHP();
+    },
+    renderHPLife: function () {
+        this.elHP.innerText = this.damageHP + ' / ' + this.defaultHP;
+    },
+    renderProgressbarHP: function () {
+        this.elProgressbarHP.style.width = (this.damageHP / this.defaultHP) * 100 + '%';
+        this.elProgressbarHP.classList.remove('low', 'critical');
+
+        if (this.damageHP > 60) {
+            // normal
+        } else if (this.damageHP > 30) {
+            this.elProgressbarHP.classList.add('low');
+        } else {
+            this.elProgressbarHP.classList.add('critical');
+        }
+    },
+    attack: function (damage) {
+        if (this.damageHP < damage) {
+            this.damageHP = 0;
+            alert('Бідний ' + this.name + ' програв бій!');
+        } else {
+            this.damageHP -= damage;
+        }
+        this.renderHP();
+    }
+};
+
 const enemy = {
     name: 'Charmander',
     defaultHP: 100,
     damageHP: 100,
     elHP: document.getElementById('health-enemy'),
     elProgressbarHP: document.getElementById('progressbar-enemy'),
+    renderHP: character.renderHP,
+    renderHPLife: character.renderHPLife,
+    renderProgressbarHP: character.renderProgressbarHP,
+    attack: character.attack
 };
 
 const enemy2 = {
@@ -23,62 +55,35 @@ const enemy2 = {
     damageHP: 100,
     elHP: document.getElementById('health-enemy2'),
     elProgressbarHP: document.getElementById('progressbar-enemy2'),
+    renderHP: character.renderHP,
+    renderHPLife: character.renderHPLife,
+    renderProgressbarHP: character.renderProgressbarHP,
+    attack: character.attack
 };
 
 $btnKick.addEventListener('click', function () {
     console.log('Kick');
-    attack(random(20), character);
-    attack(random(20), enemy);
-    attack(random(20), enemy2);
+    character.attack(random(20));
+    enemy.attack(random(20));
+    enemy2.attack(random(20));
 });
 
 $btnHeavyKick.addEventListener('click', function () {
     console.log('Heavy Kick');
-    attack(random(40), character);
-    attack(random(40), enemy);
-    attack(random(40), enemy2);
+    character.attack(random(40));
+    enemy.attack(random(40));
+    enemy2.attack(random(40));
 });
 
 function init() {
     console.log('Start game!');
-    renderHP(character);
-    renderHP(enemy);
-    renderHP(enemy2);
-}
-
-function renderHP(person) {
-    renderHPLife(person);
-    renderProgressbarHP(person);
-}
-
-function renderHPLife(person) {
-    person.elHP.innerText = person.damageHP + ' / ' + person.defaultHP;
-}
-
-function renderProgressbarHP(person) {
-    person.elProgressbarHP.style.width = (person.damageHP / person.defaultHP) * 100 + '%';
-
-    person.elProgressbarHP.classList.remove('low', 'critical');
-
-    if (person.damageHP > 60) {
-    } else if (person.damageHP > 30) {
-        person.elProgressbarHP.classList.add('low'); 
-    } else {
-        person.elProgressbarHP.classList.add('critical'); 
-    }
-}
-
-function attack(damage, person) {
-    if (person.damageHP < damage) {
-        person.damageHP = 0;
-        alert('Бідний ' + person.name + ' програв бій!');
-    } else {
-        person.damageHP -= damage;
-    }
-    renderHP(person);
+    character.renderHP();
+    enemy.renderHP();
+    enemy2.renderHP();
 }
 
 function random(num) {
     return Math.ceil(Math.random() * num);
 }
+
 init();
